5332d303f723a137bc12d058bccda4f9626b83af
dexmexter <dexter.volkman@protonmail.com>
2020-02-02 19:59:19 -0800

    remove finance information

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, stick_art, usd
+from helpers import apology, login_required
 
 # Configure application
 app = Flask(__name__)
@@ -28,9 +28,6 @@ def after_request(response):
     return response
 
 
-# Custom filter
-app.jinja_env.filters["usd"] = usd
-
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
@@ -39,10 +36,6 @@ Session(app)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
-
-# Make sure API key is set
-if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
 
 
 @app.route("/")
@@ -92,72 +85,6 @@ def index():
             total += shares * symbol_info['price']
 
     return render_template("index.html", cash=usd(cash), total=usd(total), rows=rows)
-
-
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    """Buy shares of stock"""
-    if request.method == "POST":
-        symbol_info = lookup(request.form.get("symbol"))
-        try:
-            shares = int(request.form.get("shares"))
-        except ValueError:
-            return apology("Please enter an integer.", 400)
-        cash = db.execute(
-            "SELECT cash FROM users WHERE id = :id",
-            id=session['user_id']
-        )[0]['cash']
-
-        # invalid symbol
-        if not symbol_info:
-            # TODO, add if jinja to buy.html
-            return apology("Symbol not found", 400)
-
-        # incorrect share
-        elif shares < 1:
-            return apology("Incorrect amount of shares", 400)
-
-        # process transaction
-        else:
-
-            # update cash to reflect new price
-            cash -= (symbol_info['price'] * shares)
-
-            # Apologize if user does not have enough cash for purchase
-            if cash < 0:
-                # TODO, add if jinja to buy.html that also returns current
-                # amount of cash on hand as well as how much they are trying
-                # to spend.
-                return apology("Not enough cash", 400)
-
-            else:
-                # add purchase transaction
-                db.execute(
-                    "INSERT INTO transactions (time, user_id, symbol, price, shares)\
-                    VALUES(:time,:user_id,:symbol,:price, :shares)",
-                    time=datetime.now(),
-                    user_id=session['user_id'],
-                    symbol=symbol_info['symbol'],
-                    price=symbol_info['price'],
-                    shares=shares
-                )
-
-                # update cash amount to reflect money spent
-                db.execute(
-                    "UPDATE users SET cash = :cash WHERE id = :user_id",
-                    cash=cash, user_id=session['user_id']
-                )
-
-                # redirect to home page
-                flash(
-                    "Bought {} share(s) of {}".format(shares, symbol_info['symbol'])
-                )
-                return redirect("/")
-
-    # Got to route from GET
-    else:
-        return render_template("buy.html")
 
 
 @app.route("/change", methods=["GET", "POST"])
@@ -281,28 +208,6 @@ def logout():
     return redirect("/")
 
 
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-    """Get stock quote."""
-    # Reached route via POST
-    if request.method == "POST":
-
-        symbol = request.form.get("symbol")
-        symbol_info = lookup(symbol)
-
-        if not symbol_info:
-            return apology("Not a valid symbol", 400)
-
-        else:
-            # Render page with additional message for the quoted stock price
-            return render_template("quoted.html", symbol_info=symbol_info, usd=usd(symbol_info['price']))
-
-    # Reached route via GET
-    else:
-        return render_template("quote.html")
-
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
@@ -373,177 +278,6 @@ def rps():
 
     else:
         return render_template("rps.html")
-
-
-@app.route("/hangman", methods=["GET", "POST"])
-@login_required
-def hangman():
-    """Play hangman"""
-    ## Setting up game variables ##
-
-    # Get a single random word
-    with open("./large") as f:
-        words = f.read().splitlines()
-
-    random_word = random.randrange(0, len(words))
-
-    target = words[random_word]
-
-    # exclude words with ' character
-    while "'" in target:
-        target = words[random_word]
-
-    # target word as a list of letters
-    target_list = list(target)
-
-    # set of unique letters in target
-    letters_remaining = set(target_list)
-
-    # displaying target with hidden letters
-    check_list = list("*" * len(target))
-
-    # letters that can be guessed to prevent reguessing the same letter
-    guess_pool = list(string.ascii_lowercase)
-
-    strikes = 0
-
-    # A letter has been submitted
-    if request.method == "POST":
-        guess = request.form.get("guess").lower()
-
-        # guessed more than one letter or a special character
-        if len(guess) > 1:
-            flash("Please only guess one alphabet character at a time")
-            return render_template(
-                    "hangman.html",
-                    check_list="".join(check_list),
-                    strikes=strikes,
-                    stick_art=stick_art
-                    )
-
-        # letter has already been removed from the guess_pool
-        elif guess not in guess_pool:
-            flash("That letter has already been guessed, or is not valid, please try again.")
-            return render_template(
-                    "hangman.html",
-                    check_list="".join(check_list),
-                    strikes=strikes,
-                    stick_art=stick_art
-                    )
-
-        # an incorrect guess
-        elif guess not in letters_remaining:
-            strikes += 1
-            flash("You have {} incorrect guesses remaining".format(6 - strikes))
-            return render_template(
-                    "hangman.html",
-                    check_list="".join(check_list),
-                    strikes=strikes,
-                    stick_art=stick_art
-                    )
-
-        # a correct guess
-        elif guess in letters_remaining:
-            guess_pool.remove(guess)
-            letters_remaining.remove(guess)
-
-            # get index for all matching letters
-            store_index = [i for i, x in enumerate(target_list) if x == guess]
-
-            # reveal matching letters
-            for i in store_index:
-                check_list[i] = guess
-
-        if len(letters_remaining) == 0:
-            flash("{} is the correct word. You've won!".format(target))
-            return redirect("/")
-
-        elif strikes == 6:
-            flash("Sorry that is incorrect, the word was {}".format(target))
-            return redirect("/")
-
-    # The page was loaded to initiate a game
-    else:
-        flash("Welcome to Hangman, good luck guessing the word")
-        return render_template(
-                "hangman.html",
-                check_list="".join(check_list),
-                strikes=strikes,
-                stick_art = stick_art
-                )
-
-@app.route("/battleship", methods=["GET", "POST"])
-@login_required
-def battleship():
-    """Play battleship"""
-    if request.method == "POST":
-        return apology("Not yet implemented", 400)
-
-    else:
-        return render_template("battleship.html")
-
-
-@app.route("/yacht", methods=["GET", "POST"])
-@login_required
-def yacht():
-    """Play yacht"""
-    if request.method == "POST":
-        return apology("Not yet implemented", 400)
-
-    else:
-        return render_template("yacht.html")
-
-
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    """Sell shares of stock"""
-
-    symbols_owned = db.execute(
-        "SELECT DISTINCT symbol FROM transactions WHERE user_id = :user_id",
-        user_id=session['user_id']
-    )
-
-    if request.method == "POST":
-        symbol_sold = request.form.get("symbol")
-        symbol_info = lookup(symbol_sold)
-
-        shares_sold = int(request.form.get("shares"))
-
-        shares_owned = db.execute(
-            "SELECT sum(shares) FROM transactions WHERE symbol = :symbol",
-            symbol=symbol_sold
-        )[0]['sum(shares)']
-
-        # form missing symbol or shares
-        if not symbol_sold or not shares_sold:
-            return apology("Missing data, please resubmit form.", 400)
-
-        # trying to sell too many shares
-        elif shares_sold > shares_owned:
-            return apology(
-                "You can't sell {} of {}, you own {}".format(
-                    shares_sold, symbol_sold, shares_owned
-                ), 400
-            )
-
-        else:
-            # add new transaction to db
-            db.execute(
-                "INSERT INTO transactions (time, user_id, symbol, price, shares)\
-                VALUES(:time,:user_id,:symbol,:price, :shares)",
-                time=datetime.now(),
-                user_id=session['user_id'],
-                symbol=symbol_info['symbol'],
-                price=symbol_info['price'],
-                shares=-shares_sold
-            )
-
-            # send user to home page
-            return redirect("/")
-
-    else:
-        return render_template("sell.html", symbols_owned=symbols_owned)
 
 
 def errorhandler(e):
